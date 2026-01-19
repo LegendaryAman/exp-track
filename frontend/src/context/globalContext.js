af8e252d728b1b8db439f215a8edbc289c831a96
Aman Maurya <amanmaurya8806@gmail.com>
2026-01-19 12:15:39 +0530

    Use env based API url

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 // import { getExpense } from "../../../backend/controllers/expense";
 const GlobalContext = React.createContext();
 
-const BASE_URL="http://localhost:5000/api/v1/"
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL + "/"
 
 export const GlobalProvider = ({ children }) => {
 
